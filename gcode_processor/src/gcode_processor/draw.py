08934c6d9361c1bb986f298784cbf408ed92a14d
maxsvetlik <maxsvetlik@gmail.com>
2019-05-23 14:18:51 -0500

    further modularized the gcode commander module

---
+++ gcode_processor/src/gcode_processor/draw.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2019 Svenzva Robotics
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Svenzva Robotics LLC nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+"""
+Basic script that uses the revel_gcode_commander to draw gcode files
+
+Author: Maxwell Svetlik
+"""
+
+import rospy
+import rospkg
+from gcode_processor.revel_gcode_commander import *
+
+
+if __name__=='__main__':
+        rospy.init_node('draw_gcode_node', anonymous=True)
+        rospack = rospkg.RosPack()
+        pkg_path = rospack.get_path('gcode_processor')
+        desired_file = "square_flower.nc"
+        full_path = pkg_path + "/gcode/" + desired_file
+        dgc = RevelGCodeCommander(full_path)
+        dgc.execute_gcode(0, 0.3, -0.003)
+
